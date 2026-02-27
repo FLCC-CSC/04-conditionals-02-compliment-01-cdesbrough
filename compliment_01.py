4319d6a22e7bb15a6fdf35de3b1af44d482f4b57
Corey Desbrough <cdesbrough@fingerlakes.edu>
2026-02-27 00:27:51 +0000

    compliment 01

--- compliment_01.py
+++ compliment_01.py
@@ -16,7 +16,7 @@
 ########## ENTER YER CODE BELOW THIS LINE ##########
 response = input("Would you like a compliment? ").strip()
 
-if response == "yes" or response == "y":
+if response == "yes":
     print("You have wonderful eyes.")
 
 print("Thank you for playing.")
